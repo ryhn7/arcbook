ebc09133534f8f6843c73aaa74eb945c9034b75c
Rayhan Illyas <rayhan205732@gmail.com>
2024-11-03 23:02:51 +0700

    refactor: fix overflow on button start reading

--- lib/screen/detail_screen.dart
+++ lib/screen/detail_screen.dart
@@ -7,8 +7,7 @@ class DetailScreen extends StatefulWidget {
   final BookModel bookModel;
   final Function(BookModel) onViewed;
 
-  const DetailScreen(this.bookModel,
-      {super.key, required this.onViewed});
+  const DetailScreen(this.bookModel, {super.key, required this.onViewed});
 
   @override
   State<DetailScreen> createState() => _DetailScreenState();
@@ -27,18 +26,17 @@ class _DetailScreenState extends State<DetailScreen> {
   Widget bookCoverSection() {
     return Container(
       color: cultured,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 24.0),
-        child: Center(
-          child: SizedBox(
-            height: 252,
-            width: 172,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(8),
-              child: Image.asset(
-                widget.bookModel.bookCover,
-                fit: BoxFit.fill,
-              ),
+      padding: const EdgeInsets.symmetric(vertical: 24.0),
+      child: Center(
+        child: SizedBox(
+          height: MediaQuery.of(context).size.height *
+              0.3, // Dynamically scale based on screen height
+          width: 172,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(8),
+            child: Image.asset(
+              widget.bookModel.bookCover,
+              fit: BoxFit.fill,
             ),
           ),
         ),
@@ -47,166 +45,154 @@ class _DetailScreenState extends State<DetailScreen> {
   }
 
   Widget bookInfoSection() {
-    return Expanded(
-      child: Container(
-        margin: const EdgeInsets.only(top: 16.0),
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Title and Author Row
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+    return Container(
+      margin: const EdgeInsets.only(top: 16.0),
+      padding: const EdgeInsets.symmetric(horizontal: 24.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          // Title and Author Row
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(widget.bookModel.bookTitle,
+                        maxLines: 2,
+                        style: getPoppinsSemiBoldStyle24(color: onyx)),
+                    const SizedBox(height: 4),
+                    Text(
+                      widget.bookModel.bookAuthor,
+                      overflow: TextOverflow.ellipsis,
+                      style: getPoppinsMediumStyle16(color: oldSilver),
+                    ),
+                  ],
+                ),
+              ),
+              Text(
+                'Free',
+                style: getPoppinsSemiBoldStyle20(color: seaGreen),
+              )
+            ],
+          ),
+          const SizedBox(height: 16),
+          // Rating, Pages, Language Container
+          Container(
+            padding: const EdgeInsets.symmetric(vertical: 12),
+            decoration: BoxDecoration(
+              color: cultured,
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
-                Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(widget.bookModel.bookTitle,
-                          maxLines: 2,
-                          style: getPoppinsSemiBoldStyle24(color: onyx)),
-                      const SizedBox(height: 4),
-                      Text(
-                        widget.bookModel.bookAuthor,
-                        overflow: TextOverflow.ellipsis,
-                        style: getPoppinsMediumStyle16(color: oldSilver),
-                      ),
-                    ],
+                _buildInfoItem(Icons.star,
+                    widget.bookModel.bookRating.toString(), 'Rating',
+                    iconColor: Colors.amber),
+                const SizedBox(
+                  height: 56,
+                  child: VerticalDivider(
+                    thickness: 1,
+                    color: oldSilver,
                   ),
                 ),
-                Text(
-                  'Free',
-                  style: getPoppinsSemiBoldStyle20(color: seaGreen),
-                )
+                _buildInfoItem(
+                    Icons.book, widget.bookModel.bookPages.toString(), 'Pages',
+                    iconColor: onyx),
+                const SizedBox(
+                  height: 56,
+                  child: VerticalDivider(
+                    thickness: 1,
+                    color: oldSilver,
+                  ),
+                ),
+                _buildInfoItem(
+                    Icons.language, widget.bookModel.bookLanguage, 'Language',
+                    iconColor: onyx),
               ],
             ),
-            const SizedBox(height: 16),
-            // Rating, Pages, Language Container
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 12),
-              decoration: BoxDecoration(
-                color: cultured,
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  _buildInfoItem(Icons.star,
-                      widget.bookModel.bookRating.toString(), 'Rating',
-                      iconColor: Colors.amber),
-                  const SizedBox(
-                    height: 56,
-                    child: VerticalDivider(
-                      thickness: 1,
-                      color: oldSilver,
-                    ),
-                  ),
-                  _buildInfoItem(Icons.book,
-                      widget.bookModel.bookPages.toString(), 'Pages',
-                      iconColor: onyx),
-                  const SizedBox(
-                    height: 56,
-                    child: VerticalDivider(
-                      thickness: 1,
-                      color: oldSilver,
-                    ),
-                  ),
-                  _buildInfoItem(
-                      Icons.language, widget.bookModel.bookLanguage, 'Language',
-                      iconColor: onyx),
-                ],
-              ),
+          ),
+          const SizedBox(height: 16),
+          // Description Section
+          Text(
+            'Description',
+            style: getPoppinsMediumStyle18(color: onyx),
+          ),
+          const SizedBox(height: 8),
+          Text(
+            widget.bookModel.bookDescription,
+            style: getPoppinsRegularStyle14(color: oldSilver).copyWith(
+              letterSpacing: 0.5,
+              height: 1.8,
             ),
-            const SizedBox(height: 16),
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    'Description',
-                    style: getPoppinsMediumStyle18(color: onyx),
+          ),
+          const SizedBox(height: 32),
+          // Bottom Buttons Row
+          Row(
+            children: [
+              GestureDetector(
+                onTap: () {
+                  setState(() {
+                    isBookmarked = !isBookmarked;
+                    widget.bookModel.isFavorite = isBookmarked;
+                  });
+                },
+                child: Container(
+                  width: 52.0,
+                  padding: const EdgeInsets.all(14),
+                  decoration: BoxDecoration(
+                    color: isBookmarked ? const Color(0xFFFFE6E9) : cultured,
+                    borderRadius: BorderRadius.circular(8),
                   ),
-                  const SizedBox(height: 8),
-                  Text(
-                    widget.bookModel.bookDescription,
-                    style: getPoppinsRegularStyle14(color: oldSilver).copyWith(
-                      letterSpacing: 0.5,
-                      height: 1.8,
+                  child: Image.asset(
+                    isBookmarked
+                        ? 'assets/icons/icon_bookmark_red.png'
+                        : 'assets/icons/icon_bookmark_outline.png',
+                    width: 24,
+                    height: 24,
+                  ),
+                ),
+              ),
+              const SizedBox(width: 20),
+              Expanded(
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    padding: const EdgeInsets.symmetric(vertical: 16),
+                    backgroundColor: maastrichtBlue,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16),
                     ),
                   ),
-                  const Spacer(),
-                  // Bottom Buttons Row
-                  Row(
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          setState(() {
-                            isBookmarked = !isBookmarked;
-                            widget.bookModel.isFavorite = isBookmarked;
-                          });
-                        },
-                        child: Container(
-                          width: 52.0,
-                          padding: const EdgeInsets.all(14),
-                          decoration: BoxDecoration(
-                            color: isBookmarked
-                                ? const Color(0xFFFFE6E9)
-                                : cultured,
-                            borderRadius: BorderRadius.circular(8),
-                          ),
-                          child: Image.asset(
-                            isBookmarked
-                                ? 'assets/icons/icon_bookmark_red.png'
-                                : 'assets/icons/icon_bookmark_outline.png',
-                            width: 24,
-                            height: 24,
-                          ),
+                  onPressed: () {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(
+                        content: const Text('Get ready to dive in!'),
+                        duration: const Duration(seconds: 3),
+                        behavior: SnackBarBehavior.floating,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
                         ),
-                      ),
-                      const SizedBox(width: 20),
-                      Expanded(
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            padding: const EdgeInsets.symmetric(vertical: 16),
-                            backgroundColor: maastrichtBlue,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(16),
-                            ),
-                          ),
+                        action: SnackBarAction(
+                          label: 'Dismiss',
                           onPressed: () {
-                            ScaffoldMessenger.of(context).showSnackBar(
-                              SnackBar(
-                                content: const Text('Get ready to dive in!'),
-                                duration: const Duration(seconds: 3),
-                                behavior: SnackBarBehavior.floating,
-                                shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(10),
-                                ),
-                                action: SnackBarAction(
-                                  label: 'Dismiss',
-                                  onPressed: () {
-                                    ScaffoldMessenger.of(context)
-                                        .hideCurrentSnackBar();
-                                  },
-                                ),
-                              ),
-                            );
+                            ScaffoldMessenger.of(context).hideCurrentSnackBar();
                           },
-                          child: Text(
-                            'Start Reading',
-                            style:
-                                getPoppinsSemiBoldStyle16(color: Colors.white),
-                          ),
                         ),
                       ),
-                    ],
+                    );
+                  },
+                  child: Text(
+                    'Start Reading',
+                    style: getPoppinsSemiBoldStyle16(color: Colors.white),
                   ),
-                  const SizedBox(height: 32),
-                ],
+                ),
               ),
-            ),
-          ],
-        ),
+            ],
+          ),
+          const SizedBox(height: 32),
+        ],
       ),
     );
   }
@@ -228,11 +214,15 @@ class _DetailScreenState extends State<DetailScreen> {
         ),
         centerTitle: true,
       ),
-      body: Column(
-        children: [
-          bookCoverSection(),
-          bookInfoSection(),
-        ],
+      body: SingleChildScrollView(
+        // Wrap in SingleChildScrollView for vertical scrolling
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            bookCoverSection(),
+            bookInfoSection(),
+          ],
+        ),
       ),
     );
   }
